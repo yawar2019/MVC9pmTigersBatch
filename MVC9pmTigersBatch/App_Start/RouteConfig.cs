5fba3a6753c5326227039ff01c9db8d134592e1c
yawar2019 <yawarali17@gmail.com>
2021-04-03 21:10:18 +0530

    Initial Commit

--- MVC9pmTigersBatch/App_Start/RouteConfig.cs
+++ MVC9pmTigersBatch/App_Start/RouteConfig.cs
@@ -13,11 +13,19 @@ namespace MVC9pmTigersBatch
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+               name: "Default1",
+               url: "Pissta/biryani",
+               defaults: new { controller = "New", action = "HelloWorld", id = UrlParameter.Optional }
+           );
+
+            routes.MapRoute(
+               name: "Default",
+               url: "{controller}/{action}/{id}",
+               defaults: new { controller = "New", action = "HelloWorld", id = UrlParameter.Optional }
+           );
         }
     }
 }
---
+++ MVC9pmTigersBatch/Controllers/NewController.cs
@@ -0,0 +1,81 @@
+﻿using MVC9pmTigersBatch.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC9pmTigersBatch.Controllers
+{
+    public class NewController : Controller
+    {
+       public string HelloWorld()
+        {
+            return "Hello To All";
+        }
+        public int Age()
+        {
+            return 52;
+        }
+
+        [Route("Mens/Playinggames")]
+        [Route("Boys/Playinggames")]
+        public ActionResult Toy()
+        {
+            return View("HappyTest");
+        }
+        public ActionResult Toy2()
+        {
+            return View("~/Views/Default/Index.cshtml");
+        }
+        public string Toy3(int? id)
+        {
+            return "Welcome to Employee bearing Id:" + id;//domain/controller/actionmethod/id
+        }
+        public ActionResult SendData()
+        {
+
+            EmployeeModel obj = new Models.EmployeeModel();
+            obj.EmpId = 1;
+            obj.EmpName = "Pawan";
+            obj.EmpSalary = 21000;
+
+            ViewBag.info = obj;
+
+            return View();
+        }
+
+        public ActionResult SendMultipleEmployeeData()
+        {
+
+            List<EmployeeModel> listObj = new List<EmployeeModel>();
+
+            EmployeeModel obj = new Models.EmployeeModel();
+            obj.EmpId = 1;
+            obj.EmpName = "Akshay";
+            obj.EmpSalary = 21000;
+
+
+            EmployeeModel obj1 = new Models.EmployeeModel();
+            obj1.EmpId = 2;
+            obj1.EmpName = "Vithal";
+            obj1.EmpSalary = 31000;
+
+            EmployeeModel obj2 = new Models.EmployeeModel();
+            obj2.EmpId = 3;
+            obj2.EmpName = "VT";
+            obj2.EmpSalary = 61000;
+
+            listObj.Add(obj);
+            listObj.Add(obj1);
+            listObj.Add(obj2);
+
+
+            ViewBag.info = listObj;
+        
+
+            return View();
+        }
+
+    }
+}
